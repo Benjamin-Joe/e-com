a416206644f6ce4dbd30df07a88646d78da2ccb7
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-03-19 13:18:09 +0000

    Add home url, view and templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
+"Store views"
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def all_products(request):
+    "View for all of the store's products"
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
