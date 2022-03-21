e76cbf026248dde3e4da64a2c657d88ca6ed307e
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-03-21 11:49:35 +0000

    Add home page content for store

--- store/models.py
+++ store/models.py
@@ -34,6 +34,9 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    rating = models.DecimalField(
+                                max_digits=6, decimal_places=2,
+                                null=True, blank=True)
 
     class Meta:
         "To arrange via date created"
--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
 "Store views"
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+
+
+def categories(request):
+    "View for displaing categories in dropdown list"
+    return{'categories': Category.objects.all()}
 
 
 def all_products(request):
     "View for all of the store's products"
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    "View for individual product details"
+    product = get_object_or_404(Product, slug=slug)
